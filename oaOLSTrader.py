c6aaf430f1d5688bf397dea044da5d3328bbbdc8
ajshemi <pojorshemi@gmail.com>
2019-01-10 16:34:07 -0500

    Changed file; Use cumulative regression parameters instead

--- oaOLSTrader.py
+++ oaOLSTrader.py
@@ -1,4 +1,5 @@
 #
+# Modified by AJS. This script updates the regression parameters cumulatively
 # Python Script
 # with OLS Regression-based Trading Class
 # for Oanda
@@ -7,25 +8,42 @@
 # (c) Dr. Yves J. Hilpisch
 # The Python Quants GmbH
 #
+
 import argparse
 import numpy as np
 import pandas as pd
 from tpqoa import tpqoa
 
 # parameters for signal generation ("secret sauce")
-reg = np.array([-0.02833527, -0.00774105, -0.00801869,  0.01874015,  0.00698399])
-
-
+#reg = np.array([-0.02833527, -0.00774105, -0.00801869,  0.01874015,  0.00698399])
+#
 class oaOLSTrader(tpqoa):
-    def __init__(self, conf_file, reg, instrument, units):
+    def __init__(self, conf_file, lags, instrument, units):
         tpqoa.__init__(self, conf_file)
         self.data = pd.DataFrame()
-        self.lags = len(reg)
-        self.reg = reg
+        self.lags = lags
+        #self.reg = reg
         self.instrument = instrument
         self.position = 0
         self.units = units
-        self.int_length = self.lags + 1
+        self.int_length = self.lags + 5
+
+    def create_lags(self):
+        ''' Creates return lags in the resampled DataFrame object. '''
+        self.cols = []
+        #self.cols.append('returns')
+        for lag in range(1, self.lags+1):
+            col = 'returns_%d' % lag
+            self.resam[col] = self.resam['returns'].shift(lag)
+            self.cols.append(col)
+
+    def ols_regression(self):
+        self.create_lags()
+        self.resam.dropna(inplace=True)
+        self.regr=np.linalg.lstsq(self.resam[self.cols],\
+                                  self.resam['returns'],rcond=None)[0]
+
+        self.resam['position']=np.sign(np.dot(self.resam[self.cols],self.regr))
 
     def stream_data(self, stop=None):
         ''' Starts a real-time data stream.
@@ -56,19 +74,10 @@ class oaOLSTrader(tpqoa):
                             self.create_order(self.instrument,
                                               units=self.units)
                         break
-
-    def create_lags(self):
-        ''' Creates return lags in the resampled DataFrame object. '''
-        self.cols = []
-        self.cols.append('returns')
-        for lag in range(1, self.lags):
-            col = 'returns_%d' % lag
-            self.resam[col] = self.resam['returns'].shift(lag)
-            self.cols.append(col)
-
-    def on_success(self, time, bid, ask):
+    
+    def on_success(self, time, bid, ask):
         ''' Method called when new data is retrieved. '''
-        print('%3d | ' % self.ticks, time, bid, ask)
+        print('%3d | ' % self.ticks, time, bid, ask)  # print tick data as received
 
         # collecting the incoming tick data
         self.data = self.data.append(pd.DataFrame({'bid': bid, 'ask': ask,
@@ -76,47 +85,53 @@ class oaOLSTrader(tpqoa):
                                                   index=[pd.Timestamp(time)]))
 
         # resampling the tick data to a homogeneous time interval
-        self.resam = self.data.resample('1min', label='right').last().ffill()
+        self.resam = self.data.resample('10s', label='right').last().ffill()
 
         if len(self.resam) > self.int_length:
             self.int_length = len(self.resam)
             self.resam['returns'] = np.log(self.resam['mid'] /
                                            self.resam['mid'].shift(1))
-            self.create_lags()
+            self.ols_regression()
+            if len(self.resam)%5==0:
+                print('\ninterval length %d\n' % self.int_length)
+                print(self.resam[['bid','ask','mid', 'position','returns']].tail())
+                print('\nupdated reg parameters: \n', self.regr)
+
+            #self.create_lags()
 
             # deriving position forecast
-            self.resam.dropna(inplace=True)
-            self.resam['position'] = np.sign(np.dot(self.resam[self.cols],
-                                                    self.reg))
+            #self.resam.dropna(inplace=True)
+            #reg = np.linalg.lstsq(self.resam[self.cols], self.resam['returns'])[0]
+            #self.resam['position'] = np.sign(np.dot(self.resam[self.cols],reg))
 
-            print('\ninterval length %d\n' % self.int_length)
-            print(self.resam[['mid', 'position']].tail())
+
 
             # from neutral to long
-            if self.position == 0 and self.resam['position'].iloc[-2] > 0:
+            if self.position == 0 and self.resam['position'].iloc[-2] > 0 and len(self.resam)>11:
                 self.create_order(self.instrument, units=self.units)
                 self.position = 1
 
             # from short to long
-            elif self.position == -1 and self.resam['position'].iloc[-2] > 0:
+            elif self.position == -1 and self.resam['position'].iloc[-2] > 0 and len(self.resam)>11:
                 self.create_order(self.instrument, units=2 * self.units)
                 self.position = 1
 
             # from neutral to short
-            elif self.position == 0 and self.resam['position'].iloc[-2] < 0:
+            elif self.position == 0 and self.resam['position'].iloc[-2] < 0 and len(self.resam)>11:
                 self.create_order(self.instrument, units=-self.units)
-                self.position = -1
-
-            # from long to short
-            elif self.position == 1 and self.resam['position'].iloc[-2] < 0:
+           
+           # from long to short
+            elif self.position == 1 and self.resam['position'].iloc[-2] < 0 and len(self.resam)>11:
                 self.create_order(self.instrument, units=-2 * self.units)
                 self.position = -1
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('lags', help='number of lags to do regression')
     parser.add_argument('instrument', help='instrument to parse data for')
     parser.add_argument('units', help='number of units to trade')
     args = parser.parse_args()
-    ols = oaOLSTrader('pyalgo_sample.cfg', reg, args.instrument, int(args.units))
-    ols.stream_data(stop=200)
+    ols = oaOLSTrader('pyalgo_sample.cfg', int(args.lags), args.instrument, int(args.units))
+    ols.stream_data(stop=100)
+
